ffd634531fe6c972498d9d3c2528ea89ceacf775
ITALO GABRIEL MESQUITA PEREI <20272538@cfp127aluno.local>
2021-02-08 11:12:47 -0300

    Canstrução inteface gráfica

--- Academia/src/br/senai/sp/jandira/gui/FrameAcademia.java
+++ Academia/src/br/senai/sp/jandira/gui/FrameAcademia.java
@@ -1,6 +1,9 @@
 package br.senai.sp.jandira.gui;
 
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class FrameAcademia {
 	///////////////////////////////////
@@ -9,13 +12,85 @@ public class FrameAcademia {
 	
 	public void criarTela(){
 		
-		// contrução tela 
+		//  Tela 
 		JFrame tela = new JFrame();
-		tela.setTitle("Registro Academia X-monstro");
-		tela.setSize(1000, 800);
+		tela.setTitle("Registro Academia");
+		tela.setSize(1200, 700);
 		tela.setDefaultCloseOperation(3);
-		
 		tela.setLayout(null);
+		
+		// JLabel nome
+		JLabel labelNome = new JLabel();
+		labelNome.setText("Nome: ");
+		labelNome.setBounds(30, 40, 50, 30);
+		
+		//JTextFild nome
+		JTextField textNome = new JTextField();
+		textNome.setBounds(75, 40, 300, 30);
+		
+		
+		// JLabel Sexo
+			JLabel labelSexo = new JLabel();
+			labelSexo.setText("Sexo: ");
+			labelSexo.setBounds(30, 90, 50, 30);
+			
+		//JTextFild Sexo
+			JTextField textSexo = new JTextField();
+			textSexo.setBounds(75, 90, 130, 30);
+			
+		// JLabel Data
+			JLabel labelData = new JLabel();
+			labelData.setText("Data De Nascimento: ");
+			labelData.setBounds(480, 40, 130, 30);
+						
+		//JTextFild Data
+			JTextField textData = new JTextField();
+			textData.setBounds(610, 40, 100, 30);
+			
+		// JLabel Peso
+			JLabel labelPeso = new JLabel();
+			labelPeso.setText("Peso: ");
+			labelPeso.setBounds(30, 150, 130, 30);
+									
+		//JTextFild Peso
+			JTextField textPeso = new JTextField();
+			textPeso.setBounds(75, 150, 100, 30);
+			
+		// JLabel Altura
+			JLabel labelAltura = new JLabel();
+			labelAltura.setText("Peso: ");
+			labelAltura.setBounds(30, 150, 130, 30);
+									
+		//JTextFild Altura
+			JTextField textAltura = new JTextField();
+			textAltura.setBounds(75, 150, 100, 30);
+			
+			
+			
+		// JLabel Atiidade
+			JLabel labelAtividade = new JLabel();
+			labelAtividade.setText("Nivel De Atividade: ");
+			labelAtividade.setBounds(480, 90, 130, 30);
+//									
+//		//JTextFild Atividade
+//			JTextField textAtividade = new JTextField();
+//			textAtividade.setBounds(610, 40, 100, 30);
+			
+		tela.getContentPane().add(textNome);
+		tela.getContentPane().add(labelNome);
+		tela.getContentPane().add(textSexo);
+		tela.getContentPane().add(labelSexo);
+		tela.getContentPane().add(textData);
+		tela.getContentPane().add(labelData);
+		tela.getContentPane().add(textPeso);
+		tela.getContentPane().add(labelPeso);
+		tela.getContentPane().add(textAltura);
+		tela.getContentPane().add(labelAltura);
+//		tela.getContentPane().add(textAtividade);
+		tela.getContentPane().add(labelAtividade);
+		
+		
+		tela.setVisible(true);
 	}
 	
 }
